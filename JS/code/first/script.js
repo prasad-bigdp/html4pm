dcd22d4e2c1b66bd7bad6725a38995fdaad6631f
prasad-bigdp <bigdp585@gmail.com>
2025-09-09 17:51:29 +0530

    update

---
+++ JS/code/first/script.js
@@ -0,0 +1,15 @@
+/* This is 
+a multi
+line comment */
+// This is a single line comment
+// var a = 5;
+// var a = 10;
+// let b = 5;
+// let b = 10;
+let a = +prompt("Enter a value");
+let b = +prompt("Enter b value");
+let c = a + b;
+console.log("The sum is "+C)
+console.log("welcome to Easy Language, Javascript")
+
+// Find area of rectangle using inputs length and breadth

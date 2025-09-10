c1af25222b168ca7ab5c8b35628b213e752e7393
prasad-bigdp <bigdp585@gmail.com>
2025-09-10 17:43:40 +0530

    update

--- JS/code/first/script.js
+++ JS/code/first/script.js
@@ -7,9 +7,16 @@ line comment */
 // let b = 5;
 // let b = 10;
 let a = +prompt("Enter a value");
-let b = +prompt("Enter b value");
+let b = prompt("Enter b value");
+
 let c = a + b;
-console.log("The sum is "+C)
+console.log("The sum is "+c)
 console.log("welcome to Easy Language, Javascript")
 
 // Find area of rectangle using inputs length and breadth
+
+// write JS code to accept radius of a circle and print its area
+let r = +prompt("enter radius");
+const pi = 3.14;
+console.log("the area of circle " + (pi * r * r));
+

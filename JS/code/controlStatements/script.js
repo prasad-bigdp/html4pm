7eb20581705447fabb23f46eaa843113b923f750
prasad-bigdp <bigdp585@gmail.com>
2025-09-16 10:01:46 +0530

    update

---
+++ JS/code/controlStatements/script.js
@@ -0,0 +1,89 @@
+// // simple if
+// let x = +prompt("enter any value")
+// if (x>5)
+// {
+//    console.log(`${x} is greater than 5`)
+// }
+// // if--else block
+// if (x >= 18)
+// {
+//     console.log("Eligible to vote")
+// }
+// else
+// {
+//     console.log("not eligible")
+// }
+// let age = +prompt("Enter your age")
+// if (age > 0 && age <= 12)
+// {
+//     console.log("KID")
+// }
+// else if (age > 12 && age <= 20)
+// {
+//     console.log("nibba nibbi")
+// }
+// else if (age > 20 && age < 30)
+// {
+//     console.log("young")
+// }
+// else if (age >= 30 && age < 60)
+// {
+//     console.log("mature")
+// }
+// else if(age>=60)
+// {
+//     console.log("oldddd")
+// }
+
+// // nested if
+// if (age >= 20)
+// {
+//     if (gender == "M")
+//     {
+//         console.log("needs to be responsible")
+//     }
+// }
+
+// const userInput = +prompt("Enter a number and Try your luck");
+// const randomNumber = Math.ceil(Math.random() * 14);
+// if (userInput == randomNumber)
+// {
+//     console.log("you won")
+// }
+// else if(userInput > randomNumber){
+//     console.log(
+// 			`your number(${userInput}) is greater than randomNumber(${randomNumber})`
+// 		)
+// }
+// else {
+//      console.log(
+// 				`your number(${userInput}) is less than randomNumber(${randomNumber})`
+// 			)
+// }
+
+// JS code to print day of week when number of the week is given
+const day = +prompt("enter day of the week")
+switch (day)
+{
+    case 0: console.log("sunday"); break;
+    case 1: console.log("Monday"); break;
+    case 2: console.log("Tuesday"); break;
+    case 3: console.log("Wednesday"); break;
+    case 4: console.log("Thursday"); break;
+    case 5: console.log("Friday"); break;
+    case 6: console.log("Saturday"); break;
+    default: console.log("enter proper input from 0 to 6") 
+}
+
+// BUILD A BASIC CALCULATOR USING IF AND SWITCH;
+// Inputs, a = 10, b=20 , operation: add
+// Output : 30
+
+
+
+
+
+
+
+
+

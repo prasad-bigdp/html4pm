189b29ac37450fe6a69451fd9650bfee309869a5
prasad-bigdp <bigdp585@gmail.com>
2025-09-18 18:36:30 +0530

    update

---
+++ JS/code/functions/script.js
@@ -0,0 +1,91 @@
+// // function displayHello() {
+// // 	console.log("hello world")
+// // }
+// // displayHello()
+
+// // function addTwoValues (a, b)
+// // {
+// //     console.log(a + b);
+// // }
+// // addTwoValues(5, 3);
+// // addTwoValues(2)
+// // addTwoValues()
+
+// // function characters (s)
+// // {
+// //     if (typeof (s) == 'string')
+// //     {
+// //         return s.length
+// //     }
+// //     else
+// //     {
+// //         return "input only strings"
+// //     }
+// // }
+// // let value = characters("prasad")
+// // console.log(value)
+// // let value2 = characters(1234);
+// // console.log(value2);
+
+// // function sample() {
+// // 	console.log("hi")
+// // }
+// // console.log(sample)
+// // let x = function ()// function
+// //  // expression
+// // {
+// //     console.log("bye")
+// // }
+// // x()
+// // console.log(x);
+
+// // write a function that returns area of circle using parameter 'radius'
+// function areaOfCircle (r)
+// {
+//     return Math.PI * r * r;
+// }
+// let areaOfCircle2 = function (r)
+// {
+//     return Math.PI * r * r;
+// }
+// let a1 = areaOfCircle(5);
+// console.log(a1);
+// console.log(areaOfCircle);
+
+
+
+// let y = function () { console.log("hi") };
+// console.log(y);
+// function z ()
+// {
+//     console.log("hi")
+// }
+// console.log(z)
+
+// normal
+function add (a, b)
+{
+    return a + b;
+}
+console.log(add(5, 3));
+// function expression
+let add2 = function (a, b)
+{
+    return a + b;
+}
+console.log(add2('5'))
+// arrow functions
+let add3 = (a, b) => a + b;
+console.log(add3(5, 3))
+
+
+
+
+
+
+
+
+
+
+
+
